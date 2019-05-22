45a1dc20ab7319f81925c524ca1a1f1378f75547
iamrajshah <shahrajesh2113@yahoo.com>
2019-05-22 16:04:30 +0530

    Updates

---
+++ precache-manifest.953e3dbb326e0048a1b2ce2f514d2cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ce2f3445b8d5f0eab0ae4ca73160f6",
+    "url": "/react-transpose/index.html"
+  },
+  {
+    "revision": "11067d9bf2e870e064f7",
+    "url": "/react-transpose/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "84271222903f81685bb1",
+    "url": "/react-transpose/static/css/main.5f914430.chunk.css"
+  },
+  {
+    "revision": "11067d9bf2e870e064f7",
+    "url": "/react-transpose/static/js/2.2e0dbe96.chunk.js"
+  },
+  {
+    "revision": "84271222903f81685bb1",
+    "url": "/react-transpose/static/js/main.d5658e9f.chunk.js"
+  },
+  {
+    "revision": "847400b9c57169c38ff7",
+    "url": "/react-transpose/static/js/runtime~main.e2e5d51b.js"
+  }
+]);
